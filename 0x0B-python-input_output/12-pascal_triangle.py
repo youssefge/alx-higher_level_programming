72efe6a3bf67d927b1978c77551c334d069c86b3
youssefge <elboudaliyoussef@gmail.com>
2023-07-11 11:30:06 -0700

    shhs

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""pascal_traigle module."""
+"""
+Module for pascal_triangle method.
+"""
 
 
 def pascal_triangle(n):
-    """ pascal traigle class body.
     """
-    if n <= 0:
-        return []
+    returns a list of lists of integers
+        Args:
+            n (int): number of lists and digits
+        Returns: list of lists
 
-    triangles = [[1]]
-    while len(triangles) != n:
-        tri = triangles[-1]
-        tmp = [1]
-        for i in range(len(tri) - 1):
-            tmp.append(tri[i] + tri[i + 1])
-        tmp.append(1)
-        triangles.append(tmp)
-    return
+    """
+    rows = [[1 for j in range(i + 1)] for i in range(n)]
+    for n in range(n):
+        for i in range(n - 1):
+            rows[n][i + 1] = sum(rows[n - 1][i:i + 2])
+    return rows
