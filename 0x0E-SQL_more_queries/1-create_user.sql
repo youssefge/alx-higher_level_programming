de8d7eaca856d600c983c252e2de73647d742fd8
youssefge <elboudaliyoussef@gmail.com>
2023-08-16 12:23:26 -0700

    up

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,10 +1,8 @@
--- This script creates the MySQL server user user_0d_1
+-- creates the MySQL server user user_0d_1.
+   -- user_0d_1 should have all privileges on your MySQL server
+   -- password set to user_0d_1_pwd
+   -- If the user user_0d_1 already exists, script should not fail
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- flush privileges after creating a user or granting a user permissions
-FLUSH PRIVILEGES;
-
--- granting a user all permissions in root
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
